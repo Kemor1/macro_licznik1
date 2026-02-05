d3893d812836aaa4aef4ee7cd8448ae3e6af3232
Kemor <kemorpoczta@gmail.com>
2026-02-05 01:02:31 +0100

    Ignoruj bledy lintowania

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    eslint: {
+      // Ostrzeżenie: To pozwala na wdrożenie produkcyjne, nawet jeśli
+      // projekt ma błędy ESLint.
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      // Ostrzeżenie: To pozwala na wdrożenie produkcyjne, nawet jeśli
+      // projekt ma błędy TypeScript.
+      ignoreBuildErrors: true,
+    },
+  };
+  
+  module.exports = nextConfig;
